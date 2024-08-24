2626600d9c4ec1e543681a18b8f580f9b4050d34
M. Sohaib Romee <toygamer201@gmail.com>
2024-08-25 01:31:59 +0500

    add

--- Admin restorex/src/App.jsx
+++ Admin restorex/src/App.jsx
@@ -1,43 +1,30 @@
-  import React from 'react'
-import './App.css'
-import Headers from './components/header'
-import AdminSidebar from './components/adminsidebar'
+import React from 'react';
+import './App.css';
+import Headers from './components/header';
+import AdminSidebar from './components/adminsidebar';
 
-import { Router } from 'react-bootstrap-icons'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import CustomPaginationActionsTable from './pages/CustomPaginationActionTable'
-import UserTable from './pages/UserPaginationTable'
-import Dashboard from './pages/dashboard'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import CustomPaginationActionsTable from './pages/CustomPaginationActionTable';
+import UserTable from './pages/UserPaginationTable';
+import Dashboard from './pages/dashboard';
+
 function App() {
-
-
   return (
     <> 
-     <Headers/>
-   <div className='container-fluid d-flex'>
-      
-      <div className="row d-flex" >
-        
-      <AdminSidebar />
-  
-       <Routes>
-       <Route path="/product" element={<CustomPaginationActionsTable />} />
-       <Route path="/Usersdetail" element={<UserTable />} />
-       <Route path="/dashboard" element={<Dashboard />} />
-       </Routes>
-
-              
-            
-         
-        
+      <Headers/>
+      <div className='container-fluid d-flex'>
+        <div className="row d-flex">
+          <AdminSidebar />
+          <Routes>
+            <Route path="/admin" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/product" element={<CustomPaginationActionsTable />} />
+            <Route path="/Usersdetail" element={<UserTable />} />
+          </Routes>
+        </div>
       </div>
-      
-    
-  </div>
-          
-           
     </>
-  )
+  );
 }
 
-export default App
+export default App;
